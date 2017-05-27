0a31feec305a432b913e9b51811d9924ef947579
james garzon <james@experimentality.co>
2017-05-27 14:55:16 -0500

    Load database

--- Main.m
+++ Main.m
@@ -1,6 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% PHISHING - PROYECTO FINAL       %
-% Desarrollado por:               %
+% PHISHING - FINAL PROJECT        %
+% Developed by:                   %
 % James Garzón                    %
 % Yoiner Gomez                    %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -8,5 +8,9 @@
 clc
 clear all
 close all
+
+load('dataset.mat');
+X=dataset(:,1:30);
+Y=dataset(:,end);
 
 punto=input('MENU PRINCIPAL\n1. k vecinos más cercanos \n2. Random Forest \n3. Máquina de Soporte Vectorial \n4. Funciones Discriminantes Gaussianas \n5. Redes neuronales \n6. Discriminante de Fisher \n7. Correlación de Pearson \n8. SFS \nSeleccione una opción:  ');
