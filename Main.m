480a0433f7b2e26a271c1e3a76f22bd721c689d6
james garzon <james@experimentality.co>
2017-05-27 13:28:23 -0500

    Create main menu

---
+++ Main.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% PHISHING - PROYECTO FINAL       %
+% Desarrollado por:               %
+% James Garzón                    %
+% Yoiner Gomez                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc
+clear all
+close all
+
+punto=input('MENU PRINCIPAL\n1. k vecinos más cercanos \n2. Random Forest \n3. Máquina de Soporte Vectorial \n4. Funciones Discriminantes Gaussianas \n5. Redes neuronales \n6. Discriminante de Fisher \n7. Correlación de Pearson \n8. SFS \nSeleccione una opción:  ');
